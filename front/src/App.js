5406dcb8975e515fee74d90f941cbd851a399e5a
thobenayann <63640040+thobenayann@users.noreply.github.com>
2021-02-19 14:41:23 +0100

    db(fix):bug

--- front/src/App.js
+++ front/src/App.js
@@ -1,7 +1,7 @@
 // == Import npm
 import React from 'react';
 
-// == Import composants
+// == Import components
 import Header from './components/Header';
 import Main from './components/Main';
 import Footer from './components/Footer';
